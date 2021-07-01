0f2ca8830d15168c73119945ae4ec2dfedea91b9
dtwilliams10 <david.williams110913@gmail.com>
2021-07-01 15:13:34 -0400

    Updated networking and page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import HomePage from './pages/Home';
 import { BrowserRouter as Router, Link } from 'react-router-dom'
 import './App.css';
+import AboutPage from './pages/About';
+import HomePage from './pages/Home';
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <HomePage/>
+      <Link to="/" component={HomePage}>Home</Link>
+      <Link to="/about" component={AboutPage}>About</Link>
     </Router>
   );
 }
