0b49c069b9b7fbddf2b58d09c9fe75e1b122ede3
dtwilliams10 <david.williams110913@gmail.com>
2021-07-06 09:04:23 -0400

    Attemtping to use yarn again over npm.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import AboutPage from './pages/About';
 import HomePage from './pages/Home';
@@ -7,8 +7,8 @@ import HomePage from './pages/Home';
 export default function App() {
   return (
     <Router>
-      <Link to="/" component={HomePage}>Home</Link>
-      <Link to="/about" component={AboutPage}>About</Link>
+      <Route path="/" component={HomePage}>Home</Route>
+      <Route path="/about" component={AboutPage}>About</Route>
     </Router>
   );
 }
