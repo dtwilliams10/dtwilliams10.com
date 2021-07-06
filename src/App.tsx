07577e10559d4bd7e68e658798229351dd598862
dtwilliams10 <david.williams110913@gmail.com>
2021-07-06 09:57:49 -0400

    Fixed routing and added temp nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom'
 import './App.css';
 import AboutPage from './pages/About';
 import HomePage from './pages/Home';
@@ -7,8 +7,17 @@ import HomePage from './pages/Home';
 export default function App() {
   return (
     <Router>
-      <Route path="/" component={HomePage}>Home</Route>
-      <Route path="/about" component={AboutPage}>About</Route>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/about" component={AboutPage}/>
+        <Route path="/" component={HomePage}/>
+      </Switch>
     </Router>
   );
 }
