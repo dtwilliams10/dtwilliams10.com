d448005bcf3ed3959edc9914a59762a1cb79c23d
dtwilliams10 <david.williams110913@gmail.com>
2021-07-06 11:46:41 -0400

    Fixed navbar alignment. Need to pick background color

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,15 +3,13 @@ import { Link } from 'react-router-dom';
 class NavBar extends Component {
     render() {
         return (
-            <div >
-                <nav >
-                    <ul className="navbar">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/about">About</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
-                    </ul>
-                </nav>
-            </div>
+            <nav>
+                <ul className="navbar">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </nav>
             );
     }
 }
