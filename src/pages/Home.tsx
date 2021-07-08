97e3d46ccf7afd454dec2b408f4ed4d6bff7a761
dtwilliams10 <david.williams110913@gmail.com>
2021-07-08 17:58:43 -0400

    Added formatting and framework for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,36 @@ import React from 'react';
 function HomePage() {
         return (
             <div className="App-header">
-                This is the home page for dtwilliams10.com
+                <div className="flex-container">
+                    <div>
+                        <div>
+                            Top Block Left
+                        </div>
+                        <div>
+                            Top Block Right
+                        </div>
+                    </div>
+                </div>
+                <div className="flex-container">
+                    <div>
+                        <div>
+                            Middle Block Left
+                        </div>
+                        <div>
+                            Middle Block Right
+                        </div>
+                    </div>
+                </div>
+                <div className="flex-container">
+                    <div>
+                        <div className="left">
+                            Bottom Block Left
+                        </div>
+                        <div className="right">
+                            Bottom Block Right
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
