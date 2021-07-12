23078351d89e90cc8bef55004301f5b1761e414b
dtwilliams10 <david.williams110913@gmail.com>
2021-07-12 19:47:07 -0400

    Reworking homepage using ChakraUI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,30 @@
+import { Image, Box, Heading, UnorderedList, ListItem, Center } from '@chakra-ui/react';
 import React from 'react';
-import headshot from './../images/IMG_2332.jpg'
+import photo from './../images/IMG2332.jpg'
 
 function HomePage() {
         return (
-            <div className="App-header">
-                <div className="flex-container">
-                    <div>
-                        <div>
-                            <img src={headshot} alt="Tyler Williams - Software Developer" height="450vh" width="450vw"/>
-                        </div>
-                        <div>
-                            <h2>Tyler Williams - Software Developer</h2>
-                            <p></p>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex-container">
-                    <div>
-                        <div>
-                            Middle Block Left
-                        </div>
-                        <div>
-                            Middle Block Right
-                        </div>
-                    </div>
-                </div>
-                <div className="flex-container">
-                    <div>
-                        <div className="left">
-                            Bottom Block Left
-                        </div>
-                        <div className="right">
-                            Bottom Block Right
-                        </div>
-                    </div>
-                </div>
-            </div>
+          <Center>
+              <Box>
+                <Heading as="h1">
+                  Welcome to the home page of Tyler Williams - Software
+                  Developer
+                </Heading>
+                <Heading as="h3">Masters of Science in CyberSecurity - UMGC Class of 2016</Heading>
+                <Image
+                  boxSize="600px"
+                  borderRadius="full"
+                  objectFit="cover"
+                  src={photo}
+                  alt="Tyler Williams - Software Developer"
+                />
+              </Box>
+              <UnorderedList>
+                <ListItem>Driven</ListItem>
+                <ListItem>Dependable</ListItem>
+                <ListItem>Determined</ListItem>
+              </UnorderedList>
+          </Center>
         );
     }
 
