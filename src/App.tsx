42137d9f8f58a69d3fed9338a2f0acca1a18d321
dtwilliams10 <david.williams110913@gmail.com>
2021-07-12 19:18:59 -0400

    Minor formatting change to match other functions

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,19 @@ import HomePage from './pages/Home';
 import ProjectPage from './pages/Projects';
 import ContactPage from './pages/Contact';
 
-export default function App() {
+function App() {
   return (
-    <Router>
-      <NavBar/>
-      <Switch>
-        <Route path="/projects" component={ProjectPage}/>
-        <Route path="/contact" component={ContactPage}/>
-        <Route path="/about" component={AboutPage}/>
-        <Route path="/" component={HomePage}/>
-      </Switch>
-      <Footer/>
-    </Router>
-  );
+      <Router>
+        <NavBar/>
+        <Switch>
+          <Route path="/projects" component={ProjectPage}/>
+          <Route path="/contact" component={ContactPage}/>
+          <Route path="/about" component={AboutPage}/>
+          <Route path="/" component={HomePage}/>
+        </Switch>
+        <Footer/>
+      </Router>
+    );
 }
+
+export default App;
