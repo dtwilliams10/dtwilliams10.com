d5b9849bcf53c3df067dd9dc7af957e9ce35b90e
dtwilliams10 <david.williams110913@gmail.com>
2021-07-13 19:15:33 -0400

    Restructured to use ChakraUI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,33 @@
-import { Image, Box, Heading, UnorderedList, ListItem, Center } from '@chakra-ui/react';
+import { Image, Box, Heading, UnorderedList, ListItem, VStack, Center } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons'
 import React from 'react';
 import photo from './../images/IMG2332.jpg'
 
 function HomePage() {
         return (
-          <Center>
-              <Box>
-                <Heading as="h1">
-                  Welcome to the home page of Tyler Williams - Software
-                  Developer
-                </Heading>
-                <Heading as="h3">Masters of Science in CyberSecurity - UMGC Class of 2016</Heading>
-                <Image
-                  boxSize="600px"
-                  borderRadius="full"
-                  objectFit="cover"
-                  src={photo}
-                  alt="Tyler Williams - Software Developer"
-                />
-              </Box>
-              <UnorderedList>
-                <ListItem>Driven</ListItem>
-                <ListItem>Dependable</ListItem>
-                <ListItem>Determined</ListItem>
-              </UnorderedList>
-          </Center>
+            <VStack>
+                <Box>
+                    <Heading as="h1">
+                    Welcome to the home page of Tyler Williams - Software
+                    Developer
+                    </Heading>
+                    <Heading as="h3">Masters of Science in CyberSecurity - UMGC Class of 2016</Heading>
+                    <Center>
+                        <Image
+                        boxSize="600px"
+                        borderRadius="full"
+                        objectFit="cover"
+                        src={photo}
+                        alt="Tyler Williams - Software Developer"
+                        />
+                    </Center>
+                </Box>
+                <UnorderedList>
+                    <ListItem><AddIcon/> Driven</ListItem>
+                    <ListItem><AddIcon/> Dependable</ListItem>
+                    <ListItem><AddIcon/> Determined</ListItem>
+                </UnorderedList>
+            </VStack>
         );
     }
 
