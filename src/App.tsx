beb347f9cdeab9644a5218159b5ce86405120041
dtwilliams10 <david.williams110913@gmail.com>
2021-07-18 16:14:01 -0400

    Changed over to Single Page App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -11,16 +10,12 @@ import ContactPage from './pages/Contact';
 function App() {
   return (
     <div className="body">
-      <Router>
-        <Header/>
-            <Switch>
-              <Route path="/projects" component={ProjectPage}/>
-              <Route path="/contact" component={ContactPage}/>
-              <Route path="/about" component={AboutPage}/>
-              <Route path="/" component={HomePage}/>
-            </Switch>
-        <Footer/>
-      </Router>
+      <Header/>
+      <HomePage/>
+      <AboutPage/>
+      <ProjectPage/>
+      <ContactPage/>
+      <Footer/>
     </div>
     );
 }
