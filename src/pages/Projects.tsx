3c3acd2cdc86cf6de5975c94081edbe1596e6fc9
dtwilliams10 <david.williams110913@gmail.com>
2021-07-18 16:04:43 -0400

    Forgot to fix the projects page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ProjectPage()  {
         return (
-            <div className="App-header">
+            <div className="App-body">
                 This is the project page for dtwilliams10.com
             </div>
         );
