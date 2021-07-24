d6a7182ead1e5cc7540ba2f318dde65431779f9f
dtwilliams10 <david.williams110913@gmail.com>
2021-07-24 10:41:37 -0400

    wrote tests for each page component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AboutPage from './pages/About';
+import HomePage from './pages/Home'
+import ProjectPage from './pages/Projects';
+import ContactPage from './pages/Contact';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('home page loads', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<HomePage/>, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
+
+it('about page loads', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<AboutPage/>, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
+
+it('projects page loads', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<ProjectPage/>, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
+
+it('contact page loads', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<ContactPage/>, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
